416d918877282ec6f0604e16c93a973cbe305f8e
Jean Salviano <jeansalvianodev@gmail.com>
2026-01-16 19:25:29 -0300

    configuração de SSL para conexão com o banco

--- d1fitness-nf-backend/typeorm.config.ts
+++ d1fitness-nf-backend/typeorm.config.ts
@@ -2,6 +2,8 @@ import { DataSource } from 'typeorm';
 import { EnvioNotaFiscal } from './src/modulos/envio-nota-fiscal/envio-nota-fiscal.entity';
 import 'dotenv/config';
 
+const isSSL = process.env.DB_SSL === 'true';
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
@@ -9,12 +11,16 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  ssl: true,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
+
+  ssl: isSSL,
+  ...(isSSL && {
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
-  },
+  }),
+
   entities: [EnvioNotaFiscal],
   migrations: ['src/migrations/*.ts'],
   synchronize: false,
