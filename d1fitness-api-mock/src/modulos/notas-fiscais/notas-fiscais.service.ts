135b35e6a33c0765a0cb4ebc8636489540d85588
Jean Salviano <jeansalvianodev@gmail.com>
2026-01-16 14:14:03 -0300

    melhoria na estrutura do XML

--- d1fitness-api-mock/src/modulos/notas-fiscais/notas-fiscais.service.ts
+++ d1fitness-api-mock/src/modulos/notas-fiscais/notas-fiscais.service.ts
@@ -3,32 +3,51 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 @Injectable()
 export class NotasFiscaisService {
   buscarPorCodigo(codigo: string) {
-    if (codigo !== 'NF001' && codigo !== 'NF002') {
+    if (!['NF001', 'NF002'].includes(codigo)) {
       throw new NotFoundException('Nota fiscal não encontrada');
     }
+
+    const numeroNF = codigo.replace('NF', '');
+    const dataEmissao = new Date().toISOString(); // 👈 data atual
 
     return {
       codigoNotaFiscal: codigo,
       xml: `
-        <NFe>
-          <infNFe>
-            <ide>
-              <nNF>${codigo}</nNF>
-            </ide>
-            <emit>
-              <xNome>D1FITNESS</xNome>
-            </emit>
-            <dest>
-              <xNome>Cliente Teste</xNome>
-            </dest>
-            <total>
-              <ICMSTot>
-                <vNF>199.90</vNF>
-              </ICMSTot>
-            </total>
-          </infNFe>
-        </NFe>
-      `,
+<?xml version="1.0" encoding="UTF-8"?>
+<NFe xmlns="http://www.portalfiscal.inf.br/nfe">
+  <infNFe versao="4.00">
+    <ide>
+      <nNF>${numeroNF}</nNF>
+      <serie>1</serie>
+      <tpNF>1</tpNF>
+      <dhEmi>${dataEmissao}</dhEmi>
+    </ide>
+
+    <emit>
+      <xNome>D1FITNESS</xNome>
+    </emit>
+
+    <dest>
+      <xNome>Cliente Teste</xNome>
+    </dest>
+
+    <det nItem="1">
+      <prod>
+        <xProd>Plano Fitness</xProd>
+        <qCom>1</qCom>
+        <vUnCom>199.90</vUnCom>
+        <vProd>199.90</vProd>
+      </prod>
+    </det>
+
+    <total>
+      <ICMSTot>
+        <vNF>199.90</vNF>
+      </ICMSTot>
+    </total>
+  </infNFe>
+</NFe>
+      `.trim(),
     };
   }
 }
