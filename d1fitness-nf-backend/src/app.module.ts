5dda0b14c16b484f272e3e48d5bfeeedb2270661
Jean Salviano <jeansalvianodev@gmail.com>
2026-01-16 19:27:22 -0300

    adicionado dotenv para garantir leitura correta do .env

--- d1fitness-nf-backend/src/app.module.ts
+++ d1fitness-nf-backend/src/app.module.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,10 +8,21 @@ import { GeracaoDanfeModule } from './modulos/geracao-danfe/geracao-danfe.module
 import { EmailModule } from './modulos/email/email.module';
 import { EnvioNotaFiscalModule } from './modulos/envio-nota-fiscal/envio-nota-fiscal.module';
 
+const isSSL = process.env.DB_SSL === 'true';
+
+console.log('ENV CHECK', {
+  DB_USER: process.env.DB_USER,
+  DB_NAME: process.env.DB_NAME,
+  DB_SSL: process.env.DB_SSL,
+});
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
@@ -18,20 +30,25 @@ import { EnvioNotaFiscalModule } from './modulos/envio-nota-fiscal/envio-nota-fi
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
+
       autoLoadEntities: true,
       synchronize: false,
-      ssl: true,
-      extra: {
-        ssl: {
-          rejectUnauthorized: false,
+
+      ssl: isSSL,
+      ...(isSSL && {
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
         },
-      },
+      }),
     }),
+
     VendasModule,
     NotasFiscaisModule,
     GeracaoDanfeModule,
     EmailModule,
-    EnvioNotaFiscalModule
+    EnvioNotaFiscalModule,
   ],
 })
 export class AppModule {}
